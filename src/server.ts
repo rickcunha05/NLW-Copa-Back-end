00a22820521f5c06dd044971a172f83aff81394e
Henrique Cunha <106562630+rickcunha05@users.noreply.github.com>
2022-11-02 17:58:22 -0300

    Rotas da aplicação web finalizadas

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,63 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors"
-import {PrismaClient} from '@prisma/client'
+import { z } from 'zod'
+import { PrismaClient } from '@prisma/client'
+import ShortUniqueI from 'short-unique-id'
+import ShortUniqueId from "short-unique-id";
 
 //tratamento de log para saber todas as querys executadas no banco
 const prisma = new PrismaClient({
   log: ['query']
 })
 async function bootstrap() {
-    const fastify = Fastify({
-      logger: true,        
+  const fastify = Fastify({
+    logger: true,
+  })
+
+  await fastify.register(cors, {
+    origin: true,
+  })
+  //Inicio bloco de codigos referente ao  bolão
+  fastify.get('/pools/count', async () => { //Contagem de bolão criadas
+    const count = await prisma.pool.count()
+
+    return { count }
+  })
+  fastify.post('/pools', async (request, reply) => { //validação para o titulo do bolão nunca vim vazio
+    const createPoolBody = z.object({
+      title: z.string(),
     })
 
-    await fastify.register(cors, {
-      origin: true,
+    const { title } = createPoolBody.parse(request.body)
+    const generate = new ShortUniqueId({ length: 6 }) //Geração do codigo unico do bolão
+    const code = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code: String(generate()).toUpperCase()
+      }
     })
 
-    fastify.get('/pools/count', async () => {
-      const count  = await prisma.pool.count()
+    return reply.status(201).send({ code })
 
-      return {count}
-    })
+  })
+  //
+
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count()
+
+    return { count }
+  })
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count()
+
+    return { count }
+  })
 
 
 
-    await fastify.listen({port: 3333})
+  await fastify.listen({ port: 3333 })
 }
 
 bootstrap()
